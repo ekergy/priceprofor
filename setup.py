8d57987d11a9100f65b06dd507f527dd2f18fe8f
David Dejuan <david.dejuan@ekergy.es>
2014-07-08 11:45:49 +0200

    Adding a dummy change to check out the awesome-slugify package

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='priceprofor',
       author='david, hector, marrao, mundi ',
       author_email='hmarrao@ekergy.es',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['bottle','rauth','beaker','pymongo','setuptools',
-                        'awesome-slugify',
+      install_requires=['awesome-slugify','bottle','rauth','beaker','pymongo','setuptools',
                         'Jinja2>=2.7.2'],
      )
