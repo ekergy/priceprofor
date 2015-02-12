fda0f95027dfca9ecbcf20093e8a338db78ace0c
Hugo Marrao <hmarrao@ekergy.es>
2015-02-12 16:06:32 +0100

    Another try ok

--- libs/omieinfosys/__init__.py
+++ libs/omieinfosys/__init__.py
@@ -11,7 +11,7 @@ CONN_DETAILS = {'db':'omieMercadoDiario'}
 
 # db = MongoEngine()
 import os
-host = os.environ['PENSHIFT_MONGODB_IP']
+host = os.environ['OPENSHIFT_MONGODB_IP']
 #host = os.environ['MONGODB_URL'][:-1]+str(27017)+'/'
 # host="127.3.118.130"
 #host="priceprofor-ekergy.rhcloud.com"
